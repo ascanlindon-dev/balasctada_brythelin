ac3813ea56180af82dd469dd3e28cd7691f8cd5f
ascanlindon-dev <ascanlindon@gmail.com>
2025-10-23 00:10:46 +0800

    koko

--- app/models/Product.php
+++ app/models/Product.php
@@ -93,7 +93,8 @@ class Product extends Model {
      */
     public function get_active_products() {
         try {
-            return $this->db->table('products')->get_all();
+            // Only show products with stock > 0
+            return $this->db->table('products')->where('stock >', 0)->order_by('product_id', 'DESC')->get_all();
         } catch (Exception $e) {
             throw new Exception("Database error in get_active_products: " . $e->getMessage());
         }
