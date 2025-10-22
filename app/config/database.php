f5ca96bf045fbda89845b793722fd375f2ba1fca
ascanlindon-dev <ascanlindon@gmail.com>
2025-10-22 09:30:08 +0800

    po

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'craftify',
+    'username'	=> 'sql12803889',
+    'password'	=> 'wWQ7nCBcGC',
+    'database'	=> 'sql12803889',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
