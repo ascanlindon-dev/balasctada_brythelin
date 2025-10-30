e45a677118133a76b0c198165e80620d26b99cf6
ascanlindon-dev <ascanlindon@gmail.com>
2025-10-30 09:04:15 +0800

    g

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12803889',
-    'password'	=> 'wWQ7nCBcGC',
-    'database'	=> 'sql12803889',
+    'username'	=> 'sql12805133',
+    'password'	=> 'ZfaleBXmpJ',
+    'database'	=> 'sql12805133',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
